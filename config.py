4a9ebd47c0e56238377f2993cd8965e59ddd2574
malkhan-blacklock <malkhan@blacklock.io>
2026-07-07 17:21:06 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"   # AWS's own official example key format
+AWS_ACCESS_SECRET = "AKIAIOSFODNN7EXAMPLE"   # AWS's own official example key format
 STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"  # Stripe's own documented test key
